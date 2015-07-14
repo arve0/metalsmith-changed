851516dc7b7ed59b976ce76a0233cd08f647f60b
arve0 <arve.seljebu@gmail.com>
2015-07-15 01:22:31 +0200

    shortcut mm.any if forcePattern not set

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ module.exports = function(options){
 
     var statSrc, statDst;
     Object.keys(files).forEach(function(file){ // async
-      if (mm.any(file, forcePattern)) {
+      if (forcePattern && mm.any(file, forcePattern)) {
         debug('building %s', file);
         return;
       }
