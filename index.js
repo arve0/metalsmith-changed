db46db771a1fc01049c87b881ba752558eafcd65
arve0 <arve.seljebu@gmail.com>
2016-06-01 15:43:10 +0200

    fix babel build

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-const fs = require('fs');
-const path = require('path');
-const debug = require('debug')('metalsmith-changed');
-const mm = require('micromatch');
-const DEFAULTS = {
+'use strict';
+
+var fs = require('fs');
+var path = require('path');
+var debug = require('debug')('metalsmith-changed');
+var mm = require('micromatch');
+var DEFAULTS = {
   force: false,
   forcePattern: false,
   ctimes: 'metalsmith-changed-ctimes.json' // where to store ctimes
@@ -10,13 +12,13 @@ const DEFAULTS = {
 
 module.exports = function (opts) {
   opts = Object.assign({}, DEFAULTS, opts);
-  debug(`options: ${ JSON.stringify(opts) }`);
+  debug('options: ' + JSON.stringify(opts));
 
   /**
    * Return true if filename does not match `opts.forcePattern`.
    */
-  const notForced = function (filename) {
-    const pattern = opts.forcePattern;
+  var notForced = function notForced(filename) {
+    var pattern = opts.forcePattern;
     return pattern === false || !mm.isMatch(filename, pattern);
   };
 
@@ -28,16 +30,38 @@ module.exports = function (opts) {
    */
   function createCtimes(files, filename) {
     // write ctimes to input folder
-    let ctimes = {}; // { 'index.md': 1464763631540, ... }
-    let filenames = Object.keys(files);
-    for (let f of filenames) {
-      if (!hasCtime(files[f])) {
-        continue;
+    var ctimes = {}; // { 'index.md': 1464763631540, ... }
+    var filenames = Object.keys(files);
+    var _iteratorNormalCompletion = true;
+    var _didIteratorError = false;
+    var _iteratorError = undefined;
+
+    try {
+      for (var _iterator = filenames[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+        var f = _step.value;
+
+        if (!hasCtime(files[f])) {
+          continue;
+        }
+        var ctime = files[f].stats.ctime.getTime();
+        debug('ctime ' + f + ': ' + ctime);
+        ctimes[f] = ctime;
       }
-      let ctime = files[f].stats.ctime.getTime();
-      debug(`ctime ${ f }: ${ ctime }`);
-      ctimes[f] = ctime;
+    } catch (err) {
+      _didIteratorError = true;
+      _iteratorError = err;
+    } finally {
+      try {
+        if (!_iteratorNormalCompletion && _iterator.return) {
+          _iterator.return();
+        }
+      } finally {
+        if (_didIteratorError) {
+          throw _iteratorError;
+        }
+      }
     }
+
     fs.writeFileSync(filename, JSON.stringify(ctimes, null, 2));
   }
 
@@ -47,17 +71,38 @@ module.exports = function (opts) {
     if (metalsmith.clean() || opts.force || !files[opts.ctimes]) {
       debug('building all files');
     } else {
-      const prevCtimes = JSON.parse(files[opts.ctimes].contents.toString());
-      const filenames = Object.keys(files).filter(notForced);
-      for (let f of filenames) {
-        if (!hasCtime(files[f])) {
-          debug(`${ f } does not have ctime`);
-          continue;
+      var prevCtimes = JSON.parse(files[opts.ctimes].contents.toString());
+      var filenames = Object.keys(files).filter(notForced);
+      var _iteratorNormalCompletion2 = true;
+      var _didIteratorError2 = false;
+      var _iteratorError2 = undefined;
+
+      try {
+        for (var _iterator2 = filenames[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
+          var f = _step2.value;
+
+          if (!hasCtime(files[f])) {
+            debug(f + ' does not have ctime');
+            continue;
+          }
+          // file has not changed
+          if (prevCtimes[f] && files[f].stats.ctime.getTime() === prevCtimes[f]) {
+            debug('skipping ' + f);
+            delete files[f];
+          }
         }
-        // file has not changed
-        if (prevCtimes[f] && files[f].stats.ctime.getTime() === prevCtimes[f]) {
-          debug(`skipping ${ f }`);
-          delete files[f];
+      } catch (err) {
+        _didIteratorError2 = true;
+        _iteratorError2 = err;
+      } finally {
+        try {
+          if (!_iteratorNormalCompletion2 && _iterator2.return) {
+            _iterator2.return();
+          }
+        } finally {
+          if (_didIteratorError2) {
+            throw _iteratorError2;
+          }
         }
       }
     }
